d30482f696d5b8e27eb9faae358364010446dda6
Connor <connorzalud@gmail.com>
2022-12-28 12:24:03 -0500

    redid calculator logic, calculator displays number of any digit length(1,2,3,4) , stores operands and operators in array, and can calculate them (only add and subtract)

--- javascript.js
+++ javascript.js
@@ -1,41 +1,107 @@
 let x
 let y
+let operatorArray = []
 let operator
 const display = document.querySelector(".display")
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
 const add1 = document.querySelector("#add");
+const subtract = document.querySelector("#subtract")
 const equal = document.querySelector("#equal");
+let operandsArray = [];
+display.value = "0"
+const operatorsDisplay = display.value === "+"||display.value === "-"||display.value ==="*"|| display.value==="/";
 
 one.addEventListener("click", function(){
-    display.value = "1";
-    x = 1
+    if(display.value === "0"){
+        display.value = "1"
+    } else if(display.value === "+"||display.value === "-"||display.value ==="*"|| display.value==="/"){
+        display.value = "1"
+    } 
+    else{
+        display.value = display.value + "1"
+    }
+    
 })
 
 two.addEventListener("click", function(){
-    display.value = "2";
-    y = 2
+    if(display.value === "0"){
+        display.value = "2"
+    }else if(display.value === "+"||display.value === "-"||display.value ==="*"|| display.value==="/"){
+        display.value = "2"
+    }  
+    else{
+        display.value = display.value + "2"
+    }
+   
+})
+
+three.addEventListener("click", function(){
+    if(display.value === "0"){
+        display.value = "3"
+    }else if(display.value === "+"||display.value === "-"||display.value ==="*"|| display.value==="/"){
+        display.value = "3"
+    }  
+    
+    else{
+        display.value = display.value + "3"
+    }
+    
+})
+
+four.addEventListener("click", function(){
+    if(display.value === "0"){
+        display.value = "4"
+    } else if(display.value === "+"||display.value === "-"||display.value ==="*"|| display.value==="/"){
+        display.value = "4"
+    } 
+    else{
+        display.value = display.value + "4"
+    }
+   
 })
 
 add1.addEventListener("click", function(){
-    display.value = "+"
-    operator = "+"
+
+    operand = parseInt(display.value);
+    operandsArray.push(operand);
+    display.value = "+";
+    operator = display.value;
+    operatorArray.push(operator)
+
+})
+
+subtract.addEventListener("click",function(){
+    operand = parseInt(display.value);
+    operandsArray.push(operand)
+    display.value="-";
+    operator = display.value;
+    operatorArray.push(operator);
 })
 
 equal.addEventListener("click", function(){
-    operate(x,operator,y);
-    display.value = result; 
+    operand = parseInt(display.value);
+    operandsArray.push(operand);
+    operate2(operatorArray, operandsArray);
+    operatorArray =[];
+    operandsArray=[];
 
 })
 
 
 
-function add(x,y){
-    answer = x + y;
+
+function add(operands){
+    answer = 0
+    for(i = 0; i<operands.length; i++){
+        answer = answer + operands[i]
+    }
     return answer
 }
 
-function subtract(x,y){
+function subtract1(x,y){
     answer = x - y;
     return answer
 }
@@ -59,7 +125,20 @@ function operate(x, operator, y){
        result = multiply(x,y)
     } else if(operator === "/"){
        result = divide(x,y)
-    }
+    };
     return result
+}
+
+function operate2(operatorArray, operandsArray){
+    let result = operandsArray[0]
+     for(i=0; i<operatorArray.length; i++){
+        if(operatorArray[i]=== "+"){
+            result += operandsArray[i+1]
+        } else if (operatorArray[i]==="-"){
+            result -= operandsArray[i+1]
+        }
+     }
+
+     return console.log(result)
 }
 
