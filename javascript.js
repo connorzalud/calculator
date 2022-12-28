40c5b649cc0f356b159eba026b14dfdb7cd3e821
Connor <connorzalud@gmail.com>
2022-12-28 09:54:16 -0500

    wrote functions for calculators

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
 
+
+
+function add(x,y){
+    answer = x + y;
+    return answer
+}
+
+function subtract(x,y){
+    answer = x - y;
+    return answer
+}
+
+function multiply(x,y){
+    answer = x * y;
+    return answer
+}
+
+function divide(x,y){
+    answer = x/y;
+    return answer
+}
+
+function operate(x, operator, y){
+    if(operator === "+"){
+        add(x,y)
+    } else if(operator ==="-"){
+        subtract(x,y)
+    } else if(operator === "*"){
+        multiply(x,y)
+    } else if(operator === "/"){
+        divide(x,y)
+    }
+}
+
